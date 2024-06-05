ee77864afa63bce2082a49ac01080d8b160b140f
HrEsben <esben@optus.dk>
2024-06-05 17:54:48 +0200

    Console log test

--- notes.js
+++ notes.js
@@ -4,3 +4,5 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://ibozznrkyqqfrqkripwy.supabase.co";
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+console.log("Testing availabilty");
