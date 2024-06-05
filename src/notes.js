1893496dd4b6f79c7ce603ac5c7dea05094ebeda
HrEsben <esben@optus.dk>
2024-06-05 17:59:01 +0200

    Moving files to /src

--- src/notes.js
+++ src/notes.js
@@ -1,8 +1,10 @@
 "use strict";
 
+console.log("Testing availabilty 1 ");
+
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://ibozznrkyqqfrqkripwy.supabase.co";
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-console.log("Testing availabilty");
+console.log("Testing availabilty 2");
