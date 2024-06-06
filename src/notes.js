dd34f56c634e409024467379cd7152b24e099bcc
HrEsben <esben@optus.dk>
2024-06-06 12:26:16 +0200

    Supabase cl test

--- src/notes.js
+++ src/notes.js
@@ -2,9 +2,10 @@
 
 console.log("Testing availabilty 1 ");
 
-// import { createClient } from "@supabase/supabase-js";
-// const supabaseUrl = "https://ibozznrkyqqfrqkripwy.supabase.co";
-// const supabaseKey = process.env.SUPABASE_KEY;
-// const supabase = createClient(supabaseUrl, supabaseKey);
+import { createClient } from "/node_modules/@supabase/supabase-js";
+const supabaseUrl = "https://ibozznrkyqqfrqkripwy.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 console.log("Testing availabilty 2");
+console.log(supabase);
