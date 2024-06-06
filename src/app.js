b60f96511fc18c1fda491977701bf9856c4407c4
HrEsben <esben@optus.dk>
2024-06-06 17:34:17 +0200

    Notesblok version 0.1 uden dotenv

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import dotenv from "dotenv";
+// import dotenv from "dotenv"; // Kun til lokal server
 import * as SBFunctions from "/src/functions.js";
 import { SupabaseClient } from "@supabase/supabase-js";
 import { SupabaseAuthClient } from "@supabase/supabase-js/dist/module/lib/SupabaseAuthClient";
