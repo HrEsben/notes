dbb8092addde62579a926a4957b164d7327d3113
HrEsben <esben@optus.dk>
2024-06-06 12:57:08 +0200

    Node.js initialized and dotenv installed. Gitignore added.

--- src/notes.js
+++ src/notes.js
@@ -1,11 +1,25 @@
 "use strict";
 
-console.log("Testing availabilty 1 ");
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
 
-import { createClient } from "/node_modules/@supabase/supabase-js";
-const supabaseUrl = "https://ibozznrkyqqfrqkripwy.supabase.co";
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-console.log("Testing availabilty 2");
-console.log(supabase);
+const readData = async function (table, columns) {
+  const { data, error } = await supabase.from(table).select(columns);
+  if (error) {
+    console.error("Error:", error);
+  } else {
+    console.log(data);
+  }
+};
+
+// Definer variablen 'table' og 'columns'
+const table = "note"; // Udskift med dit tabelnavn
+const columns = "*"; // Udskift med de kolonner du vil hente, f.eks. 'id, name' eller '*' for alle kolonner
+
+readData(table, columns);
